dadfdfccfca84803ceb41045d13859df7d3ce913
Daniel Graham <danielgraham@Daniels-MacBook-Pro.local>
2016-10-25 15:50:26 -0700

    Upgrade ios-simple to use Appium 1.6.0
    -Replaced elementByIosUIAutomation call with elementByAccessibilityId because we use the appium-xcui-test-driver which doesn't support UIAutomation
    -Upgraded appium-version in desiredCapabilities config file (caps.js) to version 1.6

--- sample-code/examples/node/helpers/caps.js
+++ sample-code/examples/node/helpers/caps.js
@@ -1,15 +1,15 @@
 exports.ios92 = {
   browserName: '',
-  'appium-version': '1.3',
+  'appium-version': '1.6',
   platformName: 'iOS',
-  platformVersion: '9.2',
+  platformVersion: '10.0',
   deviceName: 'iPhone 5s',
   app: undefined // will be set later
 };
 
 exports.ios81 = {
   browserName: '',
-  'appium-version': '1.3',
+  'appium-version': '1.6',
   platformName: 'iOS',
   platformVersion: '8.1',
   deviceName: 'iPhone Simulator',
@@ -18,7 +18,7 @@ exports.ios81 = {
 
 exports.android18 = {
   browserName: '',
-  'appium-version': '1.3',
+  'appium-version': '1.6',
   platformName: 'Android',
   platformVersion: '4.3',
   deviceName: 'Android Emulator',
@@ -27,7 +27,7 @@ exports.android18 = {
 
 exports.android19 = {
   browserName: '',
-  'appium-version': '1.3',
+  'appium-version': '1.6',
   platformName: 'Android',
   platformVersion: '4.4.2',
   deviceName: 'Android Emulator',
@@ -36,7 +36,7 @@ exports.android19 = {
 
 exports.selendroid16 = {
   browserName: '',
-  'appium-version': '1.3',
+  'appium-version': '1.6',
   platformName: 'Android',
   platformVersion: '4.1',
   automationName: 'selendroid',
